d8d53c891962824eee4f600f22a0fb440dca1339
Maxfield Lewin <mslewin@gmail.com>
2015-07-14 11:17:00 -0400

    Create UsersController.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render :json => @user
+    else
+      render :json => @user.errors.full_messages
+    end
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
 
   validates :email, :username, uniqueness: true
   validates :password, length: {minimum: 6, allow_nil: true}
+  after_initialize :ensure_session_token
 
   attr_reader :password
 
@@ -22,5 +23,5 @@ class User < ActiveRecord::Base
     def ensure_session_token
       self.session_token ||= User.generate_session_token
     end
-
+    
 end
