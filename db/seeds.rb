6549810a6a81a28df121a1d2d8a43bc61a58188f
Maxfield Lewin <mslewin@gmail.com>
2015-07-14 16:51:24 -0400

    Delightful faker seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,16 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Faker::Config.locale = 'en-US'
+
 User.create!(username: "max", email: "max@max.max", password: "maxmax")
 
 artists = ["Lil B", "MF DOOM", "Gucci Mane", "William Shakespeare", "William Shattner", "Will Smith", "Will.I.Am"]
 
+
 artists.each do |artist|
   artist_model = Artist.create!(name: artist)
   10.times do
-    title1 = "Welcome to #{Faker::Address.city}"
-    title2 = "#{Faker::Hacker.ingverb} the #{Faker::PhoneNumber.area_code}"
-    title3 = "So #{Faker::Hacker.adjective} they call me #{Faker::App.name}"
-    artist_model.songs.build(title: [title1, title2, title3].sample)
+
+    case (1..3).to_a.sample
+    when 1
+      title = "Welcome to #{Faker::Address.city}, land of #{Faker::Commerce.department(1)}"
+    when 2
+      title = "#{Faker::Hacker.ingverb.capitalize} the #{Faker::PhoneNumber.area_code}"
+    when 3
+      title = "So #{Faker::Hacker.adjective.capitalize} they call me #{Faker::App.name}"
+    end
+
+    contents = Faker::Lorem.paragraph;
+
+    artist_model.songs.create(title: title, contents: contents)
   end
 end
