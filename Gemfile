ebd91af85d8bbd6cf79d8de19b802bc9152b8ae6
Maxfield Lewin <mslewin@gmail.com>
2015-07-14 17:23:56 -0400

    Add backbone gem, install. Remove turbolinks.

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
-gem 'turbolinks'
+gem 'backbone-on-rails'
 gem 'jbuilder', '~> 2.0'
 gem 'bcrypt', '~> 3.1.7'
 
---
+++ app/assets/javascripts/savant.js
@@ -0,0 +1,13 @@
+window.Savant = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  Savant.initialize();
+});
