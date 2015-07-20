fe8735d90b32bb1e0ece21309edd23f13f53ecc0
Maxfield Lewin <mslewin@gmail.com>
2015-07-20 11:48:59 -0400

    ShowComments view and template.

---
+++ app/assets/javascripts/views/comments/showComments.js
@@ -0,0 +1,30 @@
+Savant.Views.ShowComments = Backbone.CompositeView.extend({
+  template: JST['comments/showComments'],
+  commentSelector: ".comments-list",
+  formSelector: ".new-comment-container",
+
+  events {
+    "focus .new-comment-pseudo-input":"newComment"
+  }
+
+  initialize: function(options){
+    this.commentableInfo = options;
+    this.listenTo(this.collection, "sync", this.render)
+  },
+
+  render: function(){
+    this.$el.html(this.template());
+    this.collection.forEach( function(comment){
+      var view = new Savant.Views.ShowComment({ model: comment });
+      this.addSubview(this.commentSelector, view);
+    }.bind(this));
+
+    return this;
+  },
+
+  newComment: function(event){
+    var view = new Savant.Views.NewComment({ collection: this.collection, commentableInfo: this.commentableInfo});
+    this.addSubview(this.formSelector, view);
+  }
+
+})
